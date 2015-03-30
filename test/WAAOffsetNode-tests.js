c2ff5235a96d95406dbe607edfd8f77f2fd53c4b
Sébastien Piquemal <sebpiq@gmail.com>
2015-03-30 11:34:43 +0300

    use only one ones generator fixes #2

--- test/WAAOffsetNode-tests.js
+++ test/WAAOffsetNode-tests.js
@@ -1,4 +1,5 @@
-var utils = require('waatest').utils
+var assert = require('assert')
+  , utils = require('waatest').utils
   , WAAOffsetNode = require('../index')
 
 describe('WAAOffsetNode', function() {
@@ -25,6 +26,27 @@ describe('WAAOffsetNode', function() {
         offsetNode.offset.linearRampToValueAtTime(1, 1)
       },
       utils.makeBlock(1, 44100, [function(ch, i) { return i * 1 / 44100 }]),
+      done
+    )
+  })
+
+  it('should use the same ones generator for all WAAOffsetNodes', function(done) {
+    var offsetNode1, offsetNode2
+    utils.expectSamples(
+      function(context) {
+        offsetNode1 = new WAAOffsetNode(context)
+        offsetNode2 = new WAAOffsetNode(context)
+        assert.equal(offsetNode1._ones, offsetNode2._ones)
+
+        offsetNode1.connect(context.destination)
+        offsetNode2.connect(context.destination)
+        offsetNode1.offset.setValueAtTime(10, 0)
+        offsetNode2.offset.setValueAtTime(0.1, 0)
+      },
+      [
+        [10.1, 10.1, 10.1, 10.1, 10.1, 10.1],
+        [10.1, 10.1, 10.1, 10.1, 10.1, 10.1]
+      ],
       done
     )
   })
